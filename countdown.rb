ff0032bef0dc702e6d6620bec68f987508459566
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-29 15:42:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
 def countdown(number)
-  while number > 0
+  while number > -1
     puts "#{number} SECOND(S)!"
+    if number == 0
+      puts "HAPPY NEW YEAR!"
+    end
     number -= 1
-  end
-  if number == 0
-    puts "HAPPY NEW YEAR!"
   end
 end
